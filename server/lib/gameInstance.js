34b6e6e2c84b7d5f8db873703b390200597e75cd
Elijah Kennedy <elijahclimbs@gmail.com>
2024-06-20 15:18:34 -0400

    generated color map from simplex noise

--- server/lib/gameInstance.js
+++ server/lib/gameInstance.js
@@ -1,10 +1,12 @@
 import { simplexPositive } from './simplex2.js';
+import rgbHex from 'rgb-hex';
 
 class GameInstance {
   constructor(rows, cols) {
     this.rows = rows
     this.cols = cols
     this.players = {}
+    this.rgb = [40, 20, 52]
     this.grid = this.initializeGrid()
   }
 
@@ -15,12 +17,16 @@ class GameInstance {
       const row = []
       for (let y = 0; y < this.cols; y++){
         let result = simplexPositive(x, y, .01)
-        row.push(result)
-        console.log(result)
+        let color = this.getColor(result)
+        row.push(color)
       }
       grid.push(row)
     }
     return grid
+  }
+
+  getColor(noise){
+    return '#' + rgbHex(this.rgb[0], this.rgb[1], this.rgb[2], noise)
   }
 }
 
