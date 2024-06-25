269f8facf218d09469325ae977cc9ac599d1ef3f
Elijah Kennedy <elijahclimbs@gmail.com>
2024-06-25 14:02:03 -0400

    mobile tunneling

--- client/src/lib/setupClient.ts
+++ client/src/lib/setupClient.ts
@@ -3,7 +3,9 @@ import { v4 as uuidv4 } from 'uuid';
 
 const setupClient = () => {
   const herokuUrl = "https://thornberry-jungle-461255bc451e.herokuapp.com/"
-  const socketAddress = window.location.hostname === "localhost" ? "ws://localhost:3000" : herokuUrl
+  const tunnelUrl = "https://thornberry.loca.lt"
+  let socketAddress = window.location.hostname === "localhost" ? "ws://localhost:3000" : herokuUrl
+  if (window.location.hostname.includes("thornberry")) socketAddress = tunnelUrl
   const socket = io(socketAddress)
   
   const playerId = () => localStorage.getItem('playerId') || localStorage.setItem('playerId', uuidv4())
