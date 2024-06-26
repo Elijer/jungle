ecb735a99aa6f249e7df5efec9dbfe2eef4c4b69
Elijah Kennedy <elijahclimbs@gmail.com>
2024-06-26 15:14:13 -0400

    t

--- client/src/custom.d.ts
+++ client/src/custom.d.ts
@@ -1,31 +1,31 @@
-declare module 'three/addons/controls/OrbitControls.js';
 declare module 'three';
+// declare module 'three/addons/controls/OrbitControls.js';
 declare module 'uuid';
 
-declare module 'three/addons/postprocessing/RenderPass.js'
-declare module 'three/addons/postprocessing/RenderPixelatedPass.js';
-declare module 'three/addons/postprocessing/OutputPass.js';
+// declare module 'three/addons/postprocessing/RenderPass.js'
+// declare module 'three/addons/postprocessing/RenderPixelatedPass.js';
+// declare module 'three/addons/postprocessing/OutputPass.js';
 
-declare module 'three/examples/jsm/postprocessing/EffectComposer.js' {
-  import { WebGLRenderer, WebGLRenderTarget, Scene, Camera, ShaderMaterial } from 'three';
+// declare module 'three/examples/jsm/postprocessing/EffectComposer.js' {
+//   import { WebGLRenderer, WebGLRenderTarget, Scene, Camera, ShaderMaterial } from 'three';
 
-  export class EffectComposer {
-    constructor(renderer: WebGLRenderer, renderTarget?: WebGLRenderTarget);
-    render(delta?: number): void;
-    addPass(pass: Pass): void;
-    setSize(width: number, height: number): void;
-  }
+//   export class EffectComposer {
+//     constructor(renderer: WebGLRenderer, renderTarget?: WebGLRenderTarget);
+//     render(delta?: number): void;
+//     addPass(pass: Pass): void;
+//     setSize(width: number, height: number): void;
+//   }
 
-  export class Pass {
-    enabled: boolean;
-    needsSwap: boolean;
-    clear: boolean;
-    renderToScreen: boolean;
-    setSize(width: number, height: number): void;
-    render(renderer: WebGLRenderer, writeBuffer: WebGLRenderTarget, readBuffer: WebGLRenderTarget, deltaTime?: number, maskActive?: boolean): void;
-  }
+//   export class Pass {
+//     enabled: boolean;
+//     needsSwap: boolean;
+//     clear: boolean;
+//     renderToScreen: boolean;
+//     setSize(width: number, height: number): void;
+//     render(renderer: WebGLRenderer, writeBuffer: WebGLRenderTarget, readBuffer: WebGLRenderTarget, deltaTime?: number, maskActive?: boolean): void;
+//   }
 
-  export class RenderPass extends Pass {
-    constructor(scene: Scene, camera: Camera, overrideMaterial?: ShaderMaterial, clearColor?: string | number | null, clearAlpha?: number);
-  }
-}
+//   export class RenderPass extends Pass {
+//     constructor(scene: Scene, camera: Camera, overrideMaterial?: ShaderMaterial, clearColor?: string | number | null, clearAlpha?: number);
+//   }
+// }
