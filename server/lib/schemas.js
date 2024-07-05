ffda63ada189b8c3443593869cafe2875efca53b
Elijah Kennedy <elijahclimbs@gmail.com>
2024-07-05 11:06:47 -0400

    removed comment

--- server/lib/schemas.js
+++ server/lib/schemas.js
@@ -14,19 +14,3 @@ export const stateSchema = yup.object({
   color: yup.string(),
   layer: yup.string().required(),
 })
-
-// This is what a socket update event can return
-// export const actionResultSchema = yup.object({
-//   color: yup.string().required(),
-//   geometry: yup.string().required(),
-//   id: yup.string().required(),
-//   position: yup.object({
-//     x: yup.number().required(),
-//     y: yup.number().required()
-//   }),
-//   layer: yup.string().required(),
-// })
-
-// It would be kind of cool to unify these by never passing state,
-// But initial state is useful to pass
-// Ultimately it's necessary because the alternative is passing the entire game state in the form of individual entity updates
