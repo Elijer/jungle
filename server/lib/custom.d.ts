3343aaaa992a316d4b7a4fc3e60e50378526f820
Elijah Kennedy <elijahclimbs@gmail.com>
2024-07-05 20:43:55 -0400

    test

---
+++ server/lib/custom.d.ts
@@ -0,0 +1,4 @@
+// declare module 'three'; // can't declare this, otherwise three types won't work
+// declare module 'three/addons/controls/OrbitControls.js';
+declare module 'uuid';
+declare module '*'; // I think this wildcard basically means that...everything is declared
