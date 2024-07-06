f4048bba44ec04f98729d5988e1125b37cafda0e
Elijah Kennedy <elijahclimbs@gmail.com>
2024-07-06 11:08:11 -0400

    attempting to blockade redundant socket connections

--- server/index.js
+++ server/index.js
@@ -5,13 +5,8 @@ import gridSize from './lib/gameConfig.js';
 
 let game = new GameInstance(gridSize, gridSize)
 
-// function parseHeader(header) {
-//   for (const directive of header.split(",")[0].split(";")) {
-//     if (directive.startsWith("for=")) {
-//       return directive.substring(4);
-//     }
-//   }
-// }
+let userIp
+let activeUsers = new Set()
 
 io.on("connection", (socket) => {
 
@@ -19,7 +14,14 @@ io.on("connection", (socket) => {
   // const ipAddress = parseHeader(socket.handshake.headers["forwarded"] || "");
   // console.log("THe IP aderess is...", ipAddress);
   console.log("thing", socket.handshake.headers['x-forwarded-for'])
-  //'x-forwarded-proto' header exists on disco
+  // This exists on disco, but not locally
+  if (socket.handshake.headers['x-forwarded-for']){
+    userIp = socket.handshake.headers['x-forwarded-for']
+    activeUsers.add(userIp)
+    if (activeUsers.has(userIp)){
+      socket.disconnect()
+    }
+  }
 
   socket.on("player joined", (playerId) => {
     console.log("player", playerId.substring(0, 4) + '...', "joined")
@@ -29,6 +31,7 @@ io.on("connection", (socket) => {
     socket.emit("state", game.getState()) // sends game to player who just joined
 
     socket.on("disconnecting", async(reason) => {
+      if (userIp) activePlayers.delete(userIp)
       const playerEvent = game.playerOffline(playerId)
       if (playerEvent) socket.broadcast.emit("update", playerEvent) // sends disconnect event to all other players
     })
