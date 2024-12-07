7fd691214ce8df471de6df0db4cc97e8739ef257
Elijah Kennedy <elijahclimbs@gmail.com>
2024-12-07 10:13:30 -0500

    changed playwright test dir name to be playwright specific

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -11,7 +11,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: 'playwright_tests',
 
   /* Run tests in files in parallel */
   fullyParallel: true,
